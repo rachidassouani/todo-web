b8bcc72f645818488c961f138a5bc994d081b121
rachidassouani <rachidassouani@gmail.com>
2020-07-22 13:13:47 +0100

    Added Todo Class for Best Practice

--- src/app/components/list-todos/list-todos.component.ts
+++ src/app/components/list-todos/list-todos.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Identifiers } from '@angular/compiler';
+
+export class Todo {
+
+  constructor(public id: number,
+              public description: string,
+              public targetDate: Date,
+              public isCompleted: boolean
+              ) {
+  }
+
+}
 
 @Component({
   selector: 'app-list-todos',
@@ -10,10 +22,10 @@ export class ListTodosComponent implements OnInit {
   constructor() { }
 
   todo = {id: 1, description: 'hi there'};
-  todos = [
-    {id: 1, description: 'hi there'},
-    {id: 1, description: 'hi there'},
-    {id: 1, description: 'hi there'}
+  todos: Todo[] = [
+    new Todo(1, 'hi there', new Date, false),
+    new Todo(2, 'hi there', new Date, true),
+    new Todo(3, 'hi there', new Date, false)
   ];
   ngOnInit(): void {
   }
