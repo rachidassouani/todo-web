8f6b108b27b665dbb0c304c06be3b0d4b4b042ce
rachidassouani <rachidassouani@gmail.com>
2020-07-23 13:32:21 +0100

    Enabled Header Links Based on User Authentication Token

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HardCodedAuthenticationService } from 'src/app/services/hard-coded-authentication.service';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public hardCodedauthService: HardCodedAuthenticationService) { }
 
   ngOnInit(): void {
   }
